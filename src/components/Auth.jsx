abeb40de6be8a22f0e68798dad762fea770a347a
Robert Bucsis <rbucsis@gmail.com>
2026-04-13 14:57:57 -0600

    Initializing future components

---
+++ src/components/Auth.jsx
@@ -0,0 +1,13 @@
+function Auth({ children }) {
+    if (true) {
+        return children
+    } else {
+        return (
+            <div>
+                Not Authorized
+            </div>
+        )
+    }
+}
+
+export default Auth
---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+const config = {
+    navigation: [
+        {
+            label: "Students",
+            link: "students"
+        },
+        {
+            label: "Conatcts",
+            link: "contacts"
+        },
+        {
+            label: "Teachers",
+            link: "teachers"
+        }
+    ]
+}
+
+export default config
