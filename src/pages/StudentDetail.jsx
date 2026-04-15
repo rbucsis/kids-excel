815e3caa6311eca15308aedb14454f2b65ed2e20
Robert Bucsis <rbucsis@gmail.com>
2026-04-15 08:47:59 -0600

    Building Student Functionality

--- src/pages/StudentDetail.jsx
+++ src/pages/StudentDetail.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+function calculateAge(birthday) { // birthday is a date
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
 
 function StudentDetail() {
     const { id } = useParams();
-    const [editing, setEditing] = useState(false)
-    const [form, setForm] = useState(null)
+    const [ tab, setTab ] = useState("Family");
     const {data: student, isLoading, error } = useQuery({
         queryKey: ['students', id],
         queryFn: () => fetch("http://localhost:8000/students/"+(id)).then(res => res.json())
@@ -29,7 +34,102 @@ function StudentDetail() {
 
     return (
         <div className='p-2'>
-            { JSON.stringify(student) }
+            <h1>{ student.first_name + " " + student.last_name }</h1>
+            <h2>{ student.short_name }</h2>
+            {student.birthdate && (
+                <div>
+                    <div>
+                        Birthday: {student.birthdate}
+                    </div>
+                    <div>
+                        Age: {calculateAge(new Date(student.birthdate))}
+                    </div>
+                </div>
+            )}
+            <div className="flex justify-around">
+                <div className={"w-[200px] text-center " + (tab === 'Family' ? "border-b-2 border-[#B795E4] bg-[#B795E4] rounded-tr-md rounded-tl-md" : "")} onClick={() => setTab("Family")}>Family</div>
+                <div className={"w-[200px] text-center " + (tab === 'Food' ? "border-b-2 border-[#B795E4] bg-[#B795E4] rounded-tr-md rounded-tl-md" : "")} onClick={() => setTab("Food")}>Food</div>
+                <div className={"w-[200px] text-center " + (tab === 'Self-Care' ? "border-b-2 border-[#B795E4] bg-[#B795E4] rounded-tr-md rounded-tl-md" : "")} onClick={() => setTab("Self-Care")}>Self-Care</div>
+                <div className={"w-[200px] text-center " + (tab === 'Development' ? "border-b-2 border-[#B795E4] bg-[#B795E4] rounded-tr-md rounded-tl-md" : "")} onClick={() => setTab("Development")}>Development</div>
+            </div>
+            <div className="p-2 flex border-2 border-[#B795E4] rounded-md">
+                <div className="w-full md:flex md:flex-wrap">
+                {tab === 'Family' && (<>
+                    <div className="w-full">
+                        Languages: {student.languages?.join(", ")}
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        <h3>Family:</h3>
+                        <table className="w-full text-left">
+                            <tbody>
+                                <tr>
+                                    <th className="w-1/2 border border-b-[#B795E4] bg-[#97A5EE] rounded-tr-md rounded-tl-md">
+                                        Name
+                                    </th>
+                                    <th className="w-1/4 border border-b-[#B795E4] bg-[#97A5EE] rounded-tr-md rounded-tl-md">
+                                        Relationship
+                                    </th>
+                                    <th className="w-1/4 border border-b-[#B795E4] bg-[#97A5EE] rounded-tr-md rounded-tl-md">
+                                        Lives at Home?
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        <h3>Pets:</h3>
+                        <table className="w-full text-left">
+                            <tbody>
+                                <tr>
+                                    <th className="w-1/2 border border-b-[#B795E4] bg-[#97A5EE] rounded-tr-md rounded-tl-md">
+                                        Name
+                                    </th>
+                                    <th className="w-1/4 border border-b-[#B795E4] bg-[#97A5EE] rounded-tr-md rounded-tl-md">
+                                        Relationship
+                                    </th>
+                                    <th className="w-1/4 border border-b-[#B795E4] bg-[#97A5EE] rounded-tr-md rounded-tl-md">
+                                        Lives at Home?
+                                    </th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </>)}
+                {tab==='Food' && (<>
+                    <div className="w-full md:w-1/2 mt-2">
+                        Appetite: {student.appetite}
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        Likes: {student.food_likes?.join(", ")}
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        Dislikes: {student.food_dislikes?.join(", ")}
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        Restrictions: {student.food_restrictions?.join(", ")}
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        Self Feeding: {student.self_feeding ? "Yes" : "No"}
+                    </div>
+                    <div className="w-full md:w-1/2 mt-2">
+                        Feeding Notes: {student.feeding_notes?.join(", ")}
+                    </div>
+                    <div className="w-full">
+                        Feeding Schedule: ---TO DO---
+                    </div>
+                </>)}
+                {tab==='Self-Care' && (
+                    <div>
+                        ---TO DO---
+                    </div>
+                )}
+                {tab==='Development' && (
+                    <div>
+                        ---TO DO---
+                    </div>
+                )}
+                </div>
+            </div>
         </div>
     )
 }
